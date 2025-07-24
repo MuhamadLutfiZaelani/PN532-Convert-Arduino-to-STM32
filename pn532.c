8ee9b991452a74a2344fe78f9f5c9676f2496675
MuhamadLutfiZaelani <109505699+MuhamadLutfiZaelani@users.noreply.github.com>
2025-07-24 09:29:08 +0700

    Update pn532.c

--- pn532.c
+++ pn532.c
@@ -16,15 +16,10 @@ uint32_t pn532_get_firmware_version(pn532 *dev)
 {
     uint32_t response;
     dev->packet_buffer[0] = PN532_COMMAND_GETFIRMWAREVERSION;
-    if (dev->interface->write_command(dev->interface->context,
-                                      dev->packet_buffer, 1,
-                                      NULL, 0)) {
+    if (dev->interface->write_command(dev->interface->context, dev->packet_buffer, 1, NULL, 0)) {
         return 0;
     }
-    int16_t status = dev->interface->read_response(dev->interface->context,
-                                                   dev->packet_buffer,
-                                                   sizeof(dev->packet_buffer),
-                                                   0);
+    int16_t status = dev->interface->read_response(dev->interface->context, dev->packet_buffer, sizeof(dev->packet_buffer), 0);
     if (status < 0) {
         return 0;
     }
@@ -44,15 +39,10 @@ uint32_t pn532_read_register(pn532 *dev, uint16_t reg)
     dev->packet_buffer[0] = PN532_COMMAND_READREGISTER;
     dev->packet_buffer[1] = (reg >> 8) & 0xFF;
     dev->packet_buffer[2] = reg & 0xFF;
-    if (dev->interface->write_command(dev->interface->context,
-                                      dev->packet_buffer, 3,
-                                      NULL, 0)) {
+    if (dev->interface->write_command(dev->interface->context, dev->packet_buffer, 3, NULL, 0)) {
         return 0;
     }
-    int16_t status = dev->interface->read_response(dev->interface->context,
-                                                   dev->packet_buffer,
-                                                   sizeof(dev->packet_buffer),
-                                                   0);
+    int16_t status = dev->interface->read_response(dev->interface->context, dev->packet_buffer, sizeof(dev->packet_buffer), 0);
     if (status < 0) {
         return 0;
     }
@@ -66,15 +56,10 @@ uint32_t pn532_write_register(pn532 *dev, uint16_t reg, uint8_t val)
     dev->packet_buffer[1] = (reg >> 8) & 0xFF;
     dev->packet_buffer[2] = reg & 0xFF;
     dev->packet_buffer[3] = val;
-    if (dev->interface->write_command(dev->interface->context,
-                                      dev->packet_buffer, 4,
-                                      NULL, 0)) {
+    if (dev->interface->write_command(dev->interface->context, dev->packet_buffer, 4, NULL, 0)) {
         return 0;
     }
-    int16_t status = dev->interface->read_response(dev->interface->context,
-                                                   dev->packet_buffer,
-                                                   sizeof(dev->packet_buffer),
-                                                   0);
+    int16_t status = dev->interface->read_response(dev->interface->context, dev->packet_buffer, sizeof(dev->packet_buffer), 0);
     if (status < 0) {
         return 0;
     }
@@ -87,29 +72,19 @@ bool pn532_write_gpio(pn532 *dev, uint8_t pinstate)
     dev->packet_buffer[0] = PN532_COMMAND_WRITEGPIO;
     dev->packet_buffer[1] = PN532_GPIO_VALIDATIONBIT | pinstate;
     dev->packet_buffer[2] = 0x00;
-    if (dev->interface->write_command(dev->interface->context,
-                                      dev->packet_buffer, 3,
-                                      NULL, 0)) {
+    if (dev->interface->write_command(dev->interface->context, dev->packet_buffer, 3, NULL, 0)) {
         return false;
     }
-    return (0 < dev->interface->read_response(dev->interface->context,
-                                              dev->packet_buffer,
-                                              sizeof(dev->packet_buffer),
-                                              0));
+    return (0 < dev->interface->read_response(dev->interface->context, dev->packet_buffer, sizeof(dev->packet_buffer), 0));
 }
 
 uint8_t pn532_read_gpio(pn532 *dev)
 {
     dev->packet_buffer[0] = PN532_COMMAND_READGPIO;
-    if (dev->interface->write_command(dev->interface->context,
-                                      dev->packet_buffer, 1,
-                                      NULL, 0)) {
+    if (dev->interface->write_command(dev->interface->context, dev->packet_buffer, 1, NULL, 0)) {
         return 0;
     }
-    dev->interface->read_response(dev->interface->context,
-                                  dev->packet_buffer,
-                                  sizeof(dev->packet_buffer),
-                                  0);
+    dev->interface->read_response(dev->interface->context, dev->packet_buffer, sizeof(dev->packet_buffer), 0);
     return dev->packet_buffer[0];
 }
 
@@ -119,15 +94,10 @@ bool pn532_sam_config(pn532 *dev)
     dev->packet_buffer[1] = 0x01;
     dev->packet_buffer[2] = 0x14;
     dev->packet_buffer[3] = 0x01;
-    if (dev->interface->write_command(dev->interface->context,
-                                      dev->packet_buffer, 4,
-                                      NULL, 0)) {
+    if (dev->interface->write_command(dev->interface->context, dev->packet_buffer, 4, NULL, 0)) {
         return false;
     }
-    return (0 < dev->interface->read_response(dev->interface->context,
-                                              dev->packet_buffer,
-                                              sizeof(dev->packet_buffer),
-                                              0));
+    return (0 < dev->interface->read_response(dev->interface->context, dev->packet_buffer, sizeof(dev->packet_buffer), 0));
 }
 
 bool pn532_set_passive_activation_retries(pn532 *dev, uint8_t maxRetries)
@@ -137,15 +107,10 @@ bool pn532_set_passive_activation_retries(pn532 *dev, uint8_t maxRetries)
     dev->packet_buffer[2] = 0xFF;
     dev->packet_buffer[3] = 0x01;
     dev->packet_buffer[4] = maxRetries;
-    if (dev->interface->write_command(dev->interface->context,
-                                      dev->packet_buffer, 5,
-                                      NULL, 0)) {
+    if (dev->interface->write_command(dev->interface->context, dev->packet_buffer, 5, NULL, 0)) {
         return false;
     }
-    return (0 < dev->interface->read_response(dev->interface->context,
-                                              dev->packet_buffer,
-                                              sizeof(dev->packet_buffer),
-                                              0));
+    return (0 < dev->interface->read_response(dev->interface->context, dev->packet_buffer, sizeof(dev->packet_buffer), 0));
 }
 
 bool pn532_set_rf_field(pn532 *dev, uint8_t autoRFCA, uint8_t rFOnOff)
@@ -153,33 +118,21 @@ bool pn532_set_rf_field(pn532 *dev, uint8_t autoRFCA, uint8_t rFOnOff)
     dev->packet_buffer[0] = PN532_COMMAND_RFCONFIGURATION;
     dev->packet_buffer[1] = 1;
     dev->packet_buffer[2] = 0x00 | autoRFCA | rFOnOff;
-    if (dev->interface->write_command(dev->interface->context,
-                                      dev->packet_buffer, 3,
-                                      NULL, 0)) {
+    if (dev->interface->write_command(dev->interface->context, dev->packet_buffer, 3, NULL, 0)) {
         return false;
     }
-    return (0 < dev->interface->read_response(dev->interface->context,
-                                              dev->packet_buffer,
-                                              sizeof(dev->packet_buffer),
-                                              0));
+    return (0 < dev->interface->read_response(dev->interface->context, dev->packet_buffer, sizeof(dev->packet_buffer), 0));
 }
 
-bool pn532_read_passive_target_id(pn532 *dev, uint8_t cardbaudrate,
-                                  uint8_t *uid, uint8_t *uidLength,
-                                  uint16_t timeout)
+bool pn532_read_passive_target_id(pn532 *dev, uint8_t cardbaudrate, uint8_t *uid, uint8_t *uidLength, uint16_t timeout)
 {
     dev->packet_buffer[0] = PN532_COMMAND_INLISTPASSIVETARGET;
     dev->packet_buffer[1] = 1;
     dev->packet_buffer[2] = cardbaudrate;
-    if (dev->interface->write_command(dev->interface->context,
-                                      dev->packet_buffer, 3,
-                                      NULL, 0)) {
+    if (dev->interface->write_command(dev->interface->context, dev->packet_buffer, 3, NULL, 0)) {
         return false;
     }
-    if (dev->interface->read_response(dev->interface->context,
-                                      dev->packet_buffer,
-                                      sizeof(dev->packet_buffer),
-                                      timeout) < 0) {
+    if (dev->interface->read_response(dev->interface->context, dev->packet_buffer, sizeof(dev->packet_buffer), timeout) < 0) {
         return false;
     }
     if (dev->packet_buffer[0] != 1) {
