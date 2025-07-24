02172fcb3b80e2b0dc9a7a0040f5f64a30ac6e39
MuhamadLutfiZaelani <109505699+MuhamadLutfiZaelani@users.noreply.github.com>
2025-07-24 09:23:28 +0700

    Update pn532_interface.h

--- pn532_interface.h
+++ pn532_interface.h
@@ -10,12 +10,11 @@ extern "C" {
 typedef struct pn532_interface {
     void (*begin)(void *context);
     void (*wakeup)(void *context);
-    int8_t (*write_command)(void *context, const uint8_t *header, uint8_t hlen,
-                            const uint8_t *body, uint8_t blen);
-    int16_t (*read_response)(void *context, uint8_t *buf, uint8_t len,
-                             uint16_t timeout);
+    int8_t (*write_command)(void *context, const uint8_t *header, uint8_t hlen, const uint8_t *body, uint8_t blen);
+    int16_t (*read_response)(void *context, uint8_t *buf, uint8_t len, uint16_t timeout);
     void *context;
-} pn532_interface;
+} 
+pn532_interface;
 
 #ifdef __cplusplus
 }
